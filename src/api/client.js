03fb8f96918bab099f7de43a20f3260408fe75f9
jesseborm <jesseborm@gmail.com>
2017-07-03 15:01:41 +0200

    First commit

---
+++ src/api/client.js
@@ -0,0 +1,20 @@
+// src/api/client.js
+
+import feathers from 'feathers/client'
+import rest from 'feathers-rest/client'
+import hooks from 'feathers-hooks'
+import auth from 'feathers-authentication-client'
+import superagent from 'superagent'
+
+const host = 'http://localhost:3030'
+export const FEATHERS_TOKEN_KEY = 'MattLoveJules'
+
+const feathersClient = feathers()
+  .configure(rest(host).superagent(superagent))
+  .configure(hooks())
+  .configure(auth({
+    storage: window.localStorage,
+    storageKey: FEATHERS_TOKEN_KEY,
+  }))
+
+export default feathersClient
