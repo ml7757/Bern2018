6c83d0571b14112f51362940b7aabcacfc8e059e
Claudia413 <cmjengelsman@gmail.com>
2017-07-04 19:05:47 +0200

    Add countdown section with reserved space for counter

---
+++ src/components/CountDown.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+import "./CountDown.css";
+
+export class CountDown extends PureComponent {
+  render() {
+    return (
+      <div className="countDown">
+        <h2>COUNTING DOWN THE DAYS</h2>
+        /* Insert countdown here */
+      </div>
+    );
+  }
+}
+
+export default CountDown;
