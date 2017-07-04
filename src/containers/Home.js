24ff6257a30ce4739e13f20bb60c086ad8940d74
Claudia413 <cmjengelsman@gmail.com>
2017-07-04 16:10:02 +0200

    Add navbar and add responsive toggle to navbar

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,6 +2,7 @@
 
 import React, { PureComponent } from 'react'
 import Banner from '../components/Banner'
+import Navigation from '../components/Navigation'
 // import MapContainer from './MapContainer'
 import RSVP from '../guests/RSVP'
 
@@ -9,7 +10,8 @@ export class Home extends PureComponent {
   render() {
     return (
       <div>
-      <Banner />
+        <Banner />
+        <Navigation />
         {/* <MapContainer /> */}
         <RSVP />
       </div>
