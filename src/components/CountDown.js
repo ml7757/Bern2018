e478a19603c0430a4270c2628aeecdb3649cea14
Claudia413 <cmjengelsman@gmail.com>
2017-07-05 13:14:09 +0200

    Add date based swtich between countdown and after wedding statement

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -2,11 +2,37 @@ import React, { PureComponent } from "react";
 import "./CountDown.css";
 
 export class CountDown extends PureComponent {
+  married() {
+    const weddingDay = new Date("July 21, 2018 16:00:00");
+    const rightNow = new Date();
+
+    if (rightNow > weddingDay) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  renderContent() {
+    if (this.married()) {
+      return (
+        <div className="countDownFinished">
+          <h2>Married happily ever after</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className="countDown">
+          <h2>COUNTING DOWN THE DAYS</h2>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div className="countDown">
-        <h2>COUNTING DOWN THE DAYS</h2>
-        /* Insert countdown here */
+      <div>
+        {this.renderContent()}
       </div>
     );
   }
