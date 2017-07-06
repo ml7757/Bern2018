b5d6dbcb1744bd4d90835a8f2c4bee4aa8adba23
Claudia413 <cmjengelsman@gmail.com>
2017-07-06 17:57:05 +0200

    Add last info for footer and add link and do styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,33 @@
 import React, { PureComponent } from "react";
 import shield from "../assets/imgs/shield.png";
 import mail from "../assets/imgs/mail.png";
+import heart from "../assets/imgs/heart.png";
+import codaisseur from "../assets/imgs/codaisseur.png";
 import "./Footer.css";
 
 export class Footer extends PureComponent {
   render() {
     return (
       <div className="footer">
-        <div>
+        <div className="couple">
           <p className="newlyweds">Julia Wieser</p>
           <img className="footer-shield" src={shield} />
           <p className="newlyweds">Matthew LaPorte</p>
         </div>
-        <img className="footer-mail" src={mail} />
-        <a className="email-link" href="mailto:julesandmatt2018@gmail.com">
-          julesandmatt2018@gmail.com
-        </a>
-        <div />
-        <div />
+        <div className="email-contact">
+          <img className="footer-mail" src={mail} />
+          <a className="email-link" href="mailto:julesandmatt2018@gmail.com">
+            julesandmatt2018@gmail.com
+          </a>
+        </div>
+        <div>
+          <p className="footer-made">Made with<span>
+          <img className="footer-heart" src={heart} />
+          </span>by graduates from
+          <a href="https://www.codaisseur.com/">
+            <img className="footer-codaisseur" src={codaisseur} />
+          </a></p>
+        </div>
       </div>
     );
   }
