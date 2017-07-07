3d103dad61a287c158485ab01619f914b76cd4be
Claudia413 <cmjengelsman@gmail.com>
2017-07-07 16:21:02 +0200

    Add accomodation page and styling for it, plus link to it from homepage

--- src/guests/Accomodation.js
+++ src/guests/Accomodation.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import Banner from "../components/Banner";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import "./Accomodation.css";
 
 export class Accomodation extends PureComponent {
   render() {
@@ -9,7 +10,7 @@ export class Accomodation extends PureComponent {
       <div>
         <Banner />
         <Navigation />
-        <div>
+        <div className="accomodation-container">
           <h2 className="title">Accomodation</h2>
           <p>
             There is no denying it and given the fact that it will also be hight
@@ -24,20 +25,103 @@ export class Accomodation extends PureComponent {
           </p>
           <ul>
             <li>
-              <p>Organised transport to and from certain events will operate from the Hotel Allegro</p>
+              <p>
+                Organised transport to and from certain events will operate from
+                the Hotel Allegro
+              </p>
             </li>
             <li>
               <p>The bride and groom will also be staying a the hotel</p>
             </li>
             <li>
-              <p>There is a chance that the Gurten Music Festival will be held on the same weekend in
-            2018. This means that cheaper accommodation options, such as Airbnb, will be just as or
-            even more expensive.</p>
+              <p>
+                There is a chance that the Gurten Music Festival will be held on
+                the same weekend in
+                2018. This means that cheaper accommodation options, such as
+                Airbnb, will be just as or
+                even more expensive.
+              </p>
             </li>
           </ul>
         </div>
         <div className="hotel-rooms">
-        
+          <div className="table-header">
+            <div>
+              <p className="table-cell">Room Category</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">
+                Discounted rate incl. breakfast
+              </p>
+            </div>
+            <div>
+              <p className="table-cell no-left">
+                Discounted rate excl. breakfast
+              </p>
+            </div>
+          </div>
+          <div className="table-row">
+            <div>
+              <p className="table-cell">Economy Room</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">CHF 170 per night</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">CHF 130 per night</p>
+            </div>
+          </div>
+          <div className="table-row dark">
+            <div>
+              <p className="table-cell">Comfort Double Room with balcony</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">CHF 190 per night</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">CHF 150 per night</p>
+            </div>
+          </div>
+          <div className="table-row">
+            <div>
+              <p className="table-cell">Panorama Double Room with view</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">CHF 220 per night</p>
+            </div>
+            <div>
+              <p className="table-cell no-left">CHF 180 per night</p>
+            </div>
+          </div>
+        </div>
+        <div className="accomodation-container">
+          <div>
+            <p>
+              These prices above include free admission to the in-house casino,
+              the fitness center and most
+              importantly, a Bern Mobility Ticket, covering all travel by public
+              transport within Bern during your
+              stay. Prices exclude an additional city tax of CHF 5.30 per adult
+              per night.
+            </p>
+          </div>
+          <div className="booking-process">
+            <h5>Booking Process</h5>
+            <p>
+              Should you choose to stay at this hotel, it is advisable not to
+              wait with your reservation. In order to take advantage of these
+              rates, reservations need to be made directly with the hotel
+              reservations team via phone or email, <span>NOT</span> via their
+              website. This will also guarantee a refundable booking where
+              cancellation is possible up until 24 hours prior to your stay.
+              When you RSVP “Yes” for the wedding, you will be sent a promotion
+              code that needs to be quoted when booking with the hotel.
+            </p>
+          </div>
+          <div className="booking-process">
+            <p><span>Email:</span> reservation@kursaal-bern.ch</p>
+            <p><span>Phone number:</span> +41 31 339 55 00</p>
+          </div>
         </div>
         <Footer />
       </div>
