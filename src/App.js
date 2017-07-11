ea5b4c241c608b8e984b6caa454ee9d3a473c661
maressajoseph <maressa.joseph@gmail.com>
2017-07-11 14:55:06 +0200

    Add load error message showing on screen and rest form disables if not attending

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './assets/styles/theme'
-import Home from './containers/Home'
+import LoadErrorMessage from './components/LoadErrorMessage'
 
 class App extends PureComponent {
   static childContextTypes = {
@@ -17,6 +17,7 @@ class App extends PureComponent {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
+          <LoadErrorMessage />
           { this.props.children }
         </div>
       </MuiThemeProvider>
