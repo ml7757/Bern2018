e3950be80ddb3d46c0b82a006181bca1f34da608
Claudia413 <cmjengelsman@gmail.com>
2017-07-11 14:29:31 +0200

    fix warnings for app and make new errors for guestitem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './assets/styles/theme'
-import Home from './containers/Home'
 
 class App extends PureComponent {
   static childContextTypes = {
