9a6c8fb25d1c81966de4574cd75414993ddd05e3
ml7757 <matthew.w.laporte@gmail.com>
2017-08-05 18:01:34 +0200

    contact section lg phone queries

--- src/guests/Contact.js
+++ src/guests/Contact.js
@@ -36,7 +36,7 @@ export class Contact extends PureComponent {
           <div className="contacts">
             <div className="contact-person">
               <img src={isabelle} alt="contact for Swiss guests" className="contact-person-img" data-pin-nopin="true"/>
-              <img src={cross} alt="swiss cross" className="background cross" data-pin-nopin="true"/>
+              <img src={cross} alt="swiss cross" className="background-cross" data-pin-nopin="true"/>
               <p><span>Isabelle Schorer</span></p>
               <p><span>Email: </span><a href="mailto:isabelleschorer@hotmail.com">isabelleschorer@hotmail.com</a></p>
               <p>Can help with:</p>
@@ -47,7 +47,7 @@ export class Contact extends PureComponent {
             </div>
             <div className="contact-person">
               <img src={rafael} alt="contact for Canadian guests" className="contact-person-img" data-pin-nopin="true"/>
-              <img src={maple} alt="canadian maple leaf" className="background maple" data-pin-nopin="true"/>
+              <img src={maple} alt="canadian maple leaf" className="background-maple" data-pin-nopin="true"/>
               <p><span>Rafael Reis</span></p>
               <p><span>Email: </span><a href="mailto:rafael@changedirection.io">rafael@changedirection.io</a></p>
               <p>Can help with:</p>
