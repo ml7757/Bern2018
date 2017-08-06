2964cd08ce16f184ac5468da2ca4cc8642d3d0b5
ml7757 <matthew.w.laporte@gmail.com>
2017-08-06 09:54:29 +0200

    adjust date/time styling - where/when section

--- src/components/WhenWhere.js
+++ src/components/WhenWhere.js
@@ -13,10 +13,10 @@ export class WhenWhere extends PureComponent {
           <div className="column-margin">
             <img src={ pic1 } className="pic" data-pin-nopin="true" alt="bridge in bern" />
             <div className="textbox">
-              <div className="date"><p>Fri 20-Jul-2018</p></div>
+              <div className="date"><p>Fri, 20 Jul 2018</p></div>
               <p className="eventtitle">Meet & Greet</p>
-              <p className="date-for-phones"><span className="bold">Date:</span> Fri 20-Jul-2018</p>
-              <p><span className="bold">Time:</span> 16:00 - 19:00</p>
+              <p className="date-for-phones"><span className="bold">Date:</span> Fri, 20 Jul 2018</p>
+              <p><span className="bold">Time:</span> 4PM - 9PM</p>
               <p><span className="bold">Location:</span> Restaurant Schwellenmätteli</p>
               <p><span className="bold">Dress:</span> Casual</p>
             </div>
@@ -24,10 +24,10 @@ export class WhenWhere extends PureComponent {
           <div className="column-rest">
             <img src={ pic2 } className="pic" data-pin-nopin="true" alt="wedding rings"/>
             <div className="textbox">
-              <div className="date"><p>Sat 21-Jul-2018</p></div>
+              <div className="date"><p>Sat, 21 Jul 2018</p></div>
               <p className="eventtitle">Wedding Celebration</p>
-              <p className="date-for-phones"><span className="bold">Date:</span> Sat 21-Jul-2018</p>
-              <p><span className="bold">Time:</span> 16:00 - Late</p>
+              <p className="date-for-phones"><span className="bold">Date:</span> Sat, 21 Jul 2018</p>
+              <p><span className="bold">Time:</span> 4:30PM - Late</p>
               <p><span className="bold">Location:</span> Ostermundigen, Switzerland</p>
               <p><span className="bold">Dress:</span> Semi-formal (tie optinal)</p>
             </div>
@@ -35,10 +35,10 @@ export class WhenWhere extends PureComponent {
           <div className="column-rest">
             <img src={ pic3 } className="pic" data-pin-nopin="true" alt="hotel in bern"/>
             <div className="textbox">
-              <div className="date"><p>Sun 22-Jul-2018</p></div>
+              <div className="date"><p>Sun, 22 Jul 2018</p></div>
               <p className="eventtitle">Farewell Get Together</p>
-              <p className="date-for-phones"><span className="bold">Date:</span> Sun 22-Jul-2018</p>
-              <p><span className="bold">Time:</span> 13:00 - 17:00</p>
+              <p className="date-for-phones"><span className="bold">Date:</span> Sun, 22 Jul 2018</p>
+              <p><span className="bold">Time:</span> 1PM - 5PM</p>
               <p><span className="bold">Location:</span> Rümligen, Switzerland</p>
               <p><span className="bold">Dress:</span> Casual</p>
             </div>
