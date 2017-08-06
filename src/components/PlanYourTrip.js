424e766c48133e8f204eb6fc0b15b144d366a4a4
ml7757 <matthew.w.laporte@gmail.com>
2017-08-06 10:58:27 +0200

    add Bern to airport list

--- src/components/PlanYourTrip.js
+++ src/components/PlanYourTrip.js
@@ -25,9 +25,15 @@ export class PlanYourTrip extends PureComponent {
             </div>
             <div className="text-info" id="gifts">
                 <h5>Flights</h5>
-                <p>If you know that you will be attending, book your flight as soon as possible. It will be high season and deals are few and far between.</p>
-                <p>Although Bern has an international airport, it is quite expensive to fly to. Guests are advised to fly into these airports if travelling by air.</p>
-                <p>Zurich {"("}<span>ZRH</span>{") - "}<span className="italic">Best Option</span></p>
+                <p>If you know that you will be attending, book your flight as
+                  soon as possible. It will be high season and deals are few and
+                  far between.
+                </p>
+                <p>It is possible for guests to travel in an out of the following
+                  airports (in order of convenience):
+                </p>
+                <p>Bern-Belp {"("}<span>BRN</span>{")"}</p>
+                <p>Zurich {"("}<span>ZRH</span>{")"}</p>
                 <p>Basel {"("}<span>BSL</span>{")"}</p>
                 <p>Geneva {"("}<span>GVA</span>{")"}</p>
             </div>
